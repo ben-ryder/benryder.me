fd3b8f80b32ea85488358a8273625ee5e9e9a0e4
Ben Ryder <dev@benryder.me>
2021-07-22 21:35:40 +0100

    Adding initial header styling.

--- src/components/region/Header.js
+++ src/components/region/Header.js
@@ -10,14 +10,15 @@ const Header = () => {
     ];
 
     return (
-        <header>
-            <nav>
-                <LinkComponent url={"/"}>
-                    <p>Ben Ryder</p>
+        <header className="flex h-20 px-4">
+            <nav className="w-full flex justify-between max-w-3xl mx-auto">
+                <LinkComponent url={"/"} className="flex items-center">
+                    <i className="w-9 h-9 rounded-full bg-blue-600" />
+                    <p className="pl-2">Ben Ryder</p>
                 </LinkComponent>
-                <ul>
+                <ul className="flex justify-between items-center text-gray-500">
                     {mainMenuLinks.map((navLink) =>
-                        <li key={navLink.url}>
+                        <li key={navLink.url} className="ml-3">
                             <LinkComponent url={navLink.url} text={navLink.text}/>
                         </li>
                     )}
