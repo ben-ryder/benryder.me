9086513913bb5bd6a8eb63bc6f32cefee9f96402
Ben Ryder <dev@benryder.me>
2021-08-15 20:44:32 +0100

    Removing eslint & prettier from project due to them causing build issues.

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,6 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
-    "gatsby-plugin-eslint",
     {
       resolve: "gatsby-transformer-remark",
       options: {
