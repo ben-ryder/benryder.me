ffb271d28244b2623c281bb9f76d41a5b623ea71
Ben Ryder <dev@benryder.me>
2021-11-29 20:22:21 +0000

    content: updating job title

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,9 +16,9 @@ const IndexPage = ({data}) => {
       <main className="mt-7 mb-10 sm:mt-14 sm:mb-20">
         <div className="max-w-2xl mx-auto px-2 mb-8">
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">Hello, I&apos;m Ben Ryder</h1>
-          <p className="text-2xl text-blue-600 font-bold">Apprentice Developer</p>
+          <p className="text-2xl text-blue-600 font-bold">Junior Developer</p>
           <p className="mt-3 prose leading-normal">
-            I&apos;m an Apprentice Developer and Degree Apprentice.
+            I&apos;m a Junior Developer and Degree Apprentice.
             My day to day work is with Drupal and other web technologies.
             In my spare time I make websites, desktop apps, python games and
             anything else that catches my interest.
