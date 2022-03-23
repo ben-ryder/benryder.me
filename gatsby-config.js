c09ae2e51bfd3a836714eafda450dc516712ad3e
Ben Ryder <git@benryder.me>
2022-03-23 19:47:25 +0000

    build: adding gatsby-plugin-netlify and removing netlify-plugin-gatsby-cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-image",
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
