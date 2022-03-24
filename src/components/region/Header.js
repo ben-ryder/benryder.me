59d22436330bf399e28d6e4ecae801feb60f0af1
Ben-Ryder <git@benryder.me>
2022-03-24 17:42:44 +0000

    feat: adding noscript banner

--- src/components/region/Header.js
+++ src/components/region/Header.js
@@ -7,6 +7,7 @@ import LinkComponent from "../elements/LinkComponent";
 
 // Assets
 import { Menu as MenuIcon, X as MenuCloseIcon } from "lucide-react";
+import NoJavascriptBanner from "../NoJavascriptBanner";
 
 const Header = () => {
   const mainMenuLinks = useStaticQuery(graphql`
@@ -25,58 +26,61 @@ const Header = () => {
   const [mainMenuIsOpen, setMainMenuIsOpen] = React.useState(false);
 
   return (
-    <header
-      id="main-header"
-      className="sticky top-0 w-full z-10 flex h-20 px-4 bg-brand-background-secondary text-brand-text-secondary"
-    >
-      {mainMenuIsOpen && (
-        <Helmet
-          bodyAttributes={{
-            class: "overflow-hidden",
-          }}
-        />
-      )}
-      <nav className="w-full flex justify-between max-w-3xl mx-auto">
-        <LinkComponent url="/" className="z-20 flex items-center">
-          <i className="w-9 h-9 rounded-full bg-brand" />
-          <p className="pl-2 font-bold">Ben Ryder</p>
-        </LinkComponent>
-        <ul
-          className={classNames(
-            {
-              hidden: !mainMenuIsOpen,
-              flex: mainMenuIsOpen,
-            },
-            "fixed top-0 py-20 w-full h-full bg-brand-background-secondary left-0 items-center justify-center flex-col", // fullscreen mobile layout
-            "sm:relative sm:py-0 sm:w-auto sm:h-auto sm:bg-transparent sm:items-start  sm:justify-start sm:flex-row ", // reverting mobile layout for desktop
-            "sm:flex sm:justify-between sm:items-center" // desktop layout
-          )}
-        >
-          {mainMenuLinks.map((navLink) => (
-            <li key={navLink.url} className="ml-4 text-xl sm:text-base">
-              <LinkComponent
-                url={navLink.url}
-                text={navLink.text}
-                className="block py-5 sm:py-0 hover:underline"
-              />
-            </li>
-          ))}
-        </ul>
-        <button
-          aria-label="Toggle Main Menu"
-          className="z-20 sm:hidden"
-          onClick={() => {
-            setMainMenuIsOpen(!mainMenuIsOpen);
-          }}
-        >
-          {mainMenuIsOpen ? (
-            <MenuCloseIcon size={40} className="fill-brand-text-secondary" />
-          ) : (
-            <MenuIcon size={40} className="fill-brand-text-secondary" />
-          )}
-        </button>
-      </nav>
-    </header>
+    <>
+      <NoJavascriptBanner />
+      <header
+        id="main-header"
+        className="sticky top-0 w-full z-10 flex h-20 px-4 bg-brand-background-secondary text-brand-text-secondary"
+      >
+        {mainMenuIsOpen && (
+          <Helmet
+            bodyAttributes={{
+              class: "overflow-hidden",
+            }}
+          />
+        )}
+        <nav className="w-full flex justify-between max-w-3xl mx-auto">
+          <LinkComponent url="/" className="z-20 flex items-center">
+            <i className="w-9 h-9 rounded-full bg-brand" />
+            <p className="pl-2 font-bold">Ben Ryder</p>
+          </LinkComponent>
+          <ul
+            className={classNames(
+              {
+                hidden: !mainMenuIsOpen,
+                flex: mainMenuIsOpen,
+              },
+              "fixed top-0 py-20 w-full h-full bg-brand-background-secondary left-0 items-center justify-center flex-col", // fullscreen mobile layout
+              "sm:relative sm:py-0 sm:w-auto sm:h-auto sm:bg-transparent sm:items-start  sm:justify-start sm:flex-row ", // reverting mobile layout for desktop
+              "sm:flex sm:justify-between sm:items-center" // desktop layout
+            )}
+          >
+            {mainMenuLinks.map((navLink) => (
+              <li key={navLink.url} className="ml-4 text-xl sm:text-base">
+                <LinkComponent
+                  url={navLink.url}
+                  text={navLink.text}
+                  className="block py-5 sm:py-0 hover:underline"
+                />
+              </li>
+            ))}
+          </ul>
+          <button
+            aria-label="Toggle Main Menu"
+            className="z-20 sm:hidden"
+            onClick={() => {
+              setMainMenuIsOpen(!mainMenuIsOpen);
+            }}
+          >
+            {mainMenuIsOpen ? (
+              <MenuCloseIcon size={40} className="fill-brand-text-secondary" />
+            ) : (
+              <MenuIcon size={40} className="fill-brand-text-secondary" />
+            )}
+          </button>
+        </nav>
+      </header>
+    </>
   );
 };
 
