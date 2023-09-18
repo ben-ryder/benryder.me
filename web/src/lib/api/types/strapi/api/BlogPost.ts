91d35c1a08b1655bd46d6ffb8b65d459ad686cba
Ben Ryder <dev@benryder.me>
2023-09-18 21:27:45 +0100

    feat(cms): adding related content to blog posts

--- web/src/lib/api/types/strapi/api/BlogPost.ts
+++ web/src/lib/api/types/strapi/api/BlogPost.ts
@@ -1,4 +1,5 @@
 import { StrapiTag } from "./Tag";
+import { StrapiProject } from "./Project";
 import { ExtractNested } from "../builtins/ExtractNested";
 import { ExtractFlat } from "../builtins/ExtractFlat";
 import { RequiredBy } from "../builtins/RequiredBy";
@@ -12,6 +13,12 @@ export interface StrapiBlogPost<Populate extends string | never = never> {
       slug: string | null;
       description: string | null;
       featured: boolean | null;
+      related_blog_posts?: {
+        data: StrapiBlogPost<ExtractNested<Populate, "related_blog_posts">>[];
+      };
+      related_projects?: {
+        data: StrapiProject<ExtractNested<Populate, "related_projects">>[];
+      };
       publishedAt: string;
       createdAt: string;
       updatedAt: string;
