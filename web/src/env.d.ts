194129f7c060d79f20173a00a0663f07d011c98b
Ben-Ryder <dev@benryder.me>
2023-09-25 18:08:22 +0100

    feat: wip on contact form and captcha intergration

--- web/src/env.d.ts
+++ web/src/env.d.ts
@@ -2,5 +2,7 @@
 interface ImportMetaEnv {
 	readonly API_BASE_URL: string;
 	readonly API_TOKEN: string;
+	readonly PUBLIC_CONTACT_FORM_ENDPOINT: string
+	readonly PUBLIC_CAPTCHA_SITE_KEY: string
 	readonly LOGGER_ENABLED: string;
 }
