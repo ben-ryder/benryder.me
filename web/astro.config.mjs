496ed4c0969d05ee6e35d6b1a9e73d5db12990f4
Ben Ryder <dev@benryder.me>
2023-10-02 21:49:27 +0100

    feat(web): adding react intergration to Astro and deleting static tag pages

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        port: 4321,
-        host: "local.benryder.me"
-    }
+  server: {
+    port: 4321,
+    host: "local.benryder.me"
+  },
+  integrations: [react()]
 });
